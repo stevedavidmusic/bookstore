3a760c79909906e398a10de707a61c940c4cd704
Your Name <you@example.com>
2019-09-23 13:38:02 -0700

    Updating server for production

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const massive = require("massive");
 const app = express();
-const ac = require("./controllers/authController")
-const ic = require("./controllers/itemController")
+const ac = require("./controllers/authController");
+const ic = require("./controllers/itemController");
 
-require("dotenv").config()
+require("dotenv").config();
 
 app.use(bodyParser.json());
 
@@ -18,14 +18,13 @@ app.use(
   })
 );
 
-massive(process.env.CONNETION_STRING)
-  .then(db => {
-    console.log("Connected to DB");
-    app.set("db", db);
-  })
-  // .catch(err => console.log(err));
+massive(process.env.CONNETION_STRING).then(db => {
+  console.log("Connected to DB");
+  app.set("db", db);
+});
+// .catch(err => console.log(err));
 
-app.use( express.static( `${__dirname}/../build` ) )
+app.use(express.static(`${__dirname}/../build`));
 
 // GET
 app.get("/api/userData", ac.getUserData);
@@ -34,30 +33,29 @@ app.get("/api/logout", ac.logout);
 app.get("/api/session_info", ac.sessionInfo);
 app.get("/api/getBooks", ic.getBooks);
 app.get("/api/book/:id", ic.getBookInfo);
-app.get("/api/getCart/:id", ic.getCart)
-app.get("/api/author/:author", ic.getBooksByAuthor)
-app.get("/api/categories/:category", ic.getBooksByCategory)
-app.get("/api/recommendations/:id/:subject", ic.getRecommendations)
+app.get("/api/getCart/:id", ic.getCart);
+app.get("/api/author/:author", ic.getBooksByAuthor);
+app.get("/api/categories/:category", ic.getBooksByCategory);
+app.get("/api/recommendations/:id/:subject", ic.getRecommendations);
 
 // POST
-app.post("/api/addToCart", ic.addToCart)
-app.post("/api/stripe", ic.stripeCheckout)
+app.post("/api/addToCart", ic.addToCart);
+app.post("/api/stripe", ic.stripeCheckout);
 
 // UPDATE
-app.put("/api/updateCart", ic.updateCart)
+app.put("/api/updateCart", ic.updateCart);
 
 // DELETE
-app.delete("/api/deleteItem/:id", ic.deleteItem)
-app.delete("/api/deleteCart/:id", ic.deleteCart)
+app.delete("/api/deleteItem/:id", ic.deleteItem);
+app.delete("/api/deleteCart/:id", ic.deleteCart);
 
-const path = require('path')
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-})
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 const PORT = process.env.SERVER_PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Ready to rollout on PORT ${PORT}!`);
 });
-///
